79c6acccab4d70528bd2468311816624cf6da546
Vincent Davis <vincent@heteroskedastic.com>
2020-09-12 10:38:26 -0600

    add some function to zttt

---
+++ zttt.py
@@ -0,0 +1,61 @@
+# ZWIFT.COM TEAM TIME TRIALS
+
+import pandas as pd
+from haversine import haversine
+
+def merge_activities(path):
+    """
+    Data is imported and columns are named using the file name a field.
+    For example Altitude_John
+    :param path:
+    :return: dataframe and names
+    """
+    df = pd.DataFrame()
+    names = []
+    for a in [f for f in path if f.is_file() and f.suffix=='.csv']:
+      print("***",a)
+      df_temp = pd.read_csv(a, parse_dates= ['Date_Time']).drop(['Unnamed: 0', 'No'], axis=1)
+      name = a.stem
+      names.append(name)
+      # print(name)
+      df_temp.rename(columns={'Latitude': f'Latitude_{name}',	'Longitude': f'Longitude_{name}', 'Altitude': f'Altitude_{name}',
+                              'Speed': f'Speed_{name}', 'Heartrate': f'Heartrate_{name}', 'Cadence': f'Cadence_{name}',
+                              'Power': f'Power_{name}'}, inplace=True)
+      if len(df) == 0:
+        df = df_temp.copy()
+      else:
+        df = pd.merge(df, df_temp, on='Date_Time')
+    return df, names
+
+def rolling_metrics(df, names, hr=True, exclude=None, periods =[30, 60, 300, 1200]):
+    """
+    Calculates 30sec, 1min, 5min 20min rolling/moving averages
+    Calculates max and min of each rolling average
+    Sometimes we are missing heartrate, so we can exlude that.
+    we allow for dropped or missing data, 10%
+    :param df:
+    :return: df and dict like {name:{metric:value},}
+    """
+    stats = {}
+    for n in names:
+        nd = stats[n] = {}
+        for p in periods:
+            temp = df[60:-60][f'Power_{n}'].rolling(p, min_periods=int(p*.9)).mean()
+            nd[f'w{p}min'] = temp.min()
+            nd[f'w{p}max'] = temp.max()
+            if hr:
+                temp = df[60:-60][f'Heartrate_{n}'].rolling(p, min_periods=int(p * .9)).mean()
+                nd[f'hr{p}min'] = temp.min()
+                nd[f'hr{p}max'] = temp.max()
+    return stats
+
+def team_position(df, names):
+    from haversine import haversine
+    lat = [lat for lat in df.columns if lat.split('_')[0] == 'Latitude']
+    lon = [lon for lon in df.columns if lon.split('_')[0] == 'Longitude']
+    df['lat_center'] = df[lat].mean(axis=1)
+    df['lon_center'] = df[lon].mean(axis=1)
+    for n in names:
+        df[f'{n}_to_center'] = df[['lat_center', 'lon_center', f"Latitude_{n}", f"Longitude_{n}"]].apply(
+            lambda x: haversine((x[0], x[1]), (x[2], x[3])), axis=1)
+    return df
